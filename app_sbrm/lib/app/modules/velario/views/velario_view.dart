5fddaf646a8eb6fba59d8c1db77e991e8e6acf68
Nilton Cesar Jacintho <nilton.jacintho@gmail.com>
2024-06-15 10:31:06 -0300

    criando form de acender vela

--- app_sbrm/lib/app/modules/velario/views/velario_view.dart
+++ app_sbrm/lib/app/modules/velario/views/velario_view.dart
@@ -40,12 +40,13 @@ class VelarioView extends GetView<VelarioController> {
             switch (position.toString()) {
               case '0':
                 GoRouter.of(context).go('/home');
+              case '1':
+                _dialogBuilder(context);
+
               default:
                 print(position);
             }
           }),
-      //   },
-
       body: Container(
           decoration: const BoxDecoration(
             image: DecorationImage(
@@ -54,6 +55,43 @@ class VelarioView extends GetView<VelarioController> {
             ),
           ),
           child: null),
+    );
+  }
+
+  Future<void> _dialogBuilder(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Basic dialog title'),
+          content: const Text(
+            'A dialog is a type of modal window that\n'
+            'appears in front of app content to\n'
+            'provide critical information, or prompt\n'
+            'for a decision to be made.',
+          ),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Disable'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Enable'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
